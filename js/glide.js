dbb153e0f8f11c44ca48e73c1dc3018de7b224ed
SonyGahan <94231488+SonyGahan@users.noreply.github.com>
2023-09-27 10:04:55 -0300

    Diseño del Slide en paginas Index e Item

---
+++ js/glide.js
@@ -0,0 +1,15 @@
+new Glide('.glide', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 3,
+    gap: 30,
+    breakpoint: {
+        991: {
+            perView: 2
+        },
+        768: {
+            perView: 1
+        }
+    }
+
+}).mount();
