d79a69d31516eaf194e98696c0411561d44af44c
EMILIANOMARESCA <emaresca@adbarbieri.com>
2023-12-08 01:46:37 -0300

    Se crea el login dinamico vinculando con base de datos
    Se crea el login dinamico vinculando con base de datos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,7 +5,7 @@ const mainControllers = {
 
     index: async (req, res) => {
 		console.log('Controlador de main ejecutado');
-        let sqlProducts = "SELECT image_front, image_back, licence_name, product_name, price, CASE dues WHEN 3 THEN '3 CUOTAS SIN INTERES' WHEN 6 THEN '6 CUOTAS SIN INTERES' WHEN 9 THEN '9 CUOTAS CON INTERES' WHEN 12 THEN '12 CUOTAS CON INTERES' END 'dues' FROM funko_test.product INNER JOIN funko_test.licence ON funko_test.licence.licence_id = funko_test.product.licence_id ORDER BY product_id"
+        let sqlProducts = "SELECT image_front, image_back, licence_name, product_name, price, CASE dues WHEN 3 THEN '3 CUOTAS SIN INTERES' WHEN 6 THEN '6 CUOTAS SIN INTERES' WHEN 9 THEN '9 CUOTAS CON INTERES' WHEN 12 THEN '12 CUOTAS CON INTERES' END 'dues' FROM funko_test.product INNER JOIN funko_test.licence ON funko_test.licence.licence_id = funko_test.product.licence_id ORDER BY product_id DESC LIMIT 5"
 		let sqlLicences = 'SELECT licence_id, licence_name, licence_description, licence_image FROM funko_test.licence ORDER BY licence_id'
         try {
             const db = await connect(); // Establecer la conexión
