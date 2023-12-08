e93023d32bd41eb39f284ad9571bb39fd6d58f26
EMILIANOMARESCA <emaresca@adbarbieri.com>
2023-12-08 00:24:57 -0300

    Se convierte el index a dinamico heredando datos de la base.
    Se convierte el index a dinamico heredando datos de la base.
    
    Se agregan imagenes a la base que son los covers de cada coleccion

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -6,11 +6,14 @@ const mainControllers = {
     index: async (req, res) => {
 		console.log('Controlador de main ejecutado');
         let sqlProducts = "SELECT image_front, image_back, licence_name, product_name, price, CASE dues WHEN 3 THEN '3 CUOTAS SIN INTERES' WHEN 6 THEN '6 CUOTAS SIN INTERES' WHEN 9 THEN '9 CUOTAS CON INTERES' WHEN 12 THEN '12 CUOTAS CON INTERES' END 'dues' FROM funko_test.product INNER JOIN funko_test.licence ON funko_test.licence.licence_id = funko_test.product.licence_id ORDER BY product_id"
-		try {
+		let sqlLicences = 'SELECT licence_id, licence_name, licence_description, licence_image FROM funko_test.licence ORDER BY licence_id'
+        try {
             const db = await connect(); // Establecer la conexión
 			const [results] = await db.query(sqlProducts);
-            console.log('Resultados de la consulta:', results);
-            res.render('index', { productos: results });
+            console.log('Productos para el slider:', results);
+            const [licencias] = await db.query(sqlLicences);
+            console.log('Licencias para nuevos ingresos:', licencias);
+            res.render('index', { productos: results, licencias : licencias });
 		} catch (error) {
             console.error('Error al obtener productos:', error);
             return res.status(500).render('error', { error: 'Error al obtener productos' });
