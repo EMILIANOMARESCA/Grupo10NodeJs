c389bd1471ef375dfb617efb60c803d0761818b1
Mauro Laime <maurol.dev@gmail.com>
2023-12-12 00:25:06 -0300

    conexion de shop con la bd

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,33 +1,40 @@
-const connect = require('../config/conn'); // Asegúrate de que la ruta sea correcta
+const { getConnection } = require('../config/conn'); // Asegúrate de que la ruta sea correcta
+const path = require('path');
 require('dotenv').config();
-
-
 
 const mainControllers = {
 
     index: async (req, res) => {
-		console.log('Controlador de main ejecutado');
-        let sqlProducts = `SELECT image_front, image_back, licence_name, product_name, price, CASE dues WHEN 3 THEN '3 CUOTAS SIN INTERES' WHEN 6 THEN '6 CUOTAS SIN INTERES' WHEN 9 THEN '9 CUOTAS CON INTERES' WHEN 12 THEN '12 CUOTAS CON INTERES' END 'dues' FROM ${process.env.DB}.product INNER JOIN ${process.env.DB}.licence ON ${process.env.DB}.licence.licence_id = ${process.env.DB}.product.licence_id ORDER BY product_id DESC LIMIT 5`
-		let sqlLicences = `SELECT licence_id, licence_name, licence_description, licence_image FROM ${process.env.DB}.licence ORDER BY licence_id`
+        console.log('Controlador de main ejecutado');
+        let sqlProducts = `SELECT product_id, image_front, image_back, licence_name, product_name, price, CASE dues WHEN 3 THEN '3 CUOTAS SIN INTERES' WHEN 6 THEN '6 CUOTAS SIN INTERES' WHEN 9 THEN '9 CUOTAS CON INTERES' WHEN 12 THEN '12 CUOTAS CON INTERES' END 'dues' FROM ${process.env.DB}.product INNER JOIN ${process.env.DB}.licence ON ${process.env.DB}.licence.licence_id = ${process.env.DB}.product.licence_id ORDER BY product_id DESC LIMIT 5`;
+        let sqlLicences = `SELECT licence_id, licence_name, licence_description, licence_image FROM ${process.env.DB}.licence ORDER BY licence_id`;
+
+        let connection;
         try {
-            const db = await connect(); // Establecer la conexión
-			const [results] = await db.query(sqlProducts);
+            connection = await getConnection();
+            const [results] = await connection.query(sqlProducts);
             console.log('Productos para el slider:', results);
-            const [licencias] = await db.query(sqlLicences);
+            const [licencias] = await connection.query(sqlLicences);
             console.log('Licencias para nuevos ingresos:', licencias);
             res.render('index', { productos: results, licencias : licencias });
-		} catch (error) {
+        } catch (error) {
             console.error('Error al obtener productos:', error);
             return res.status(500).render('error', { error: 'Error al obtener productos' });
+        } finally {
+            if (connection) {
+                connection.release();
+            }
         }
     },
     
-    contact: (req, res) => res.send('Route for Contact View'),
+    contact: (req, res) => res.render(path.resolve(__dirname, '../views/components/contact.ejs'),
+    {
+        view:{
+            title: 'Contacto'
+        }
+    }),
     about: (req, res) => res.send('Route for About View'),
-    faqs: (req, res) => es.send('Route for Faqs View')
-}
+    faqs: (req, res) => res.send('Route for Faqs View')
+};
 
-
-
-//Exporto el modulo Controllers
 module.exports = mainControllers;
--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,8 +1,36 @@
-const connect = require('../config/conn'); // Asegúrate de que la ruta sea correcta
+const path = require('path');
+//const conn = require('../config/conn'); // Asegúrate de que la ruta sea correcta
+const { getAll } = require('../models/product.models');
 require('dotenv').config();
 
 const shopControllers = {
-    shop: (req, res) => res.send('Route for Shop View'),
+    /* shop: (req, res) => res.send('Route for Shop View'), */
+    
+    shop: async (req, res) => {
+        
+        const dbdata = await getAll();
+
+        if(dbdata.error)
+        {
+            res.render(path.resolve(__dirname, '../views/components/404.ejs'),
+            {
+                view:{
+                    title: 'Shop'
+                }
+            })
+        }
+        else
+        {
+            res.render(path.resolve(__dirname, '../views/shop/shop.ejs'),
+            {
+                view:{
+                    title: 'Shop'
+                },
+                items: dbdata
+            });
+        }
+        
+    },
     getItem: (req, res) => res.send('Route for find and retrieve a product from an ID View'),
     addItem: (req, res) => res.send('Route for add the current item to the Shop  cart View'),
     viewCart: (req, res) => res.send('Route for Cart View'),
